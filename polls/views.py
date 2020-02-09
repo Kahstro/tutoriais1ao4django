be6481ee171c197a5869394847aa0e1afd0d54fe
kahstro <viniciusm0r3or4@gmail.com>
2020-02-09 12:22:35 -0300

    tutorial 3

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index, by Vinicius Moreira (El Maestro).")
+    latest_question_list = Question.objects.order_by('-pubDate')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "Você está vendo o resultado da questão %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Você está votando na questão %s." % question_id)
